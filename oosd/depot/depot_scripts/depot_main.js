77b40f8de895b84b548b51455b07242bb35d599b
kavinda <kavindasr@icloud.com>
2020-03-25 03:08:34 +0530

    improved Depot interface. emp array added to js

--- oosd/depot/depot_scripts/depot_main.js
+++ oosd/depot/depot_scripts/depot_main.js
@@ -1,3 +1,17 @@
+var x=0;
+var arr = [];
+
+function adder(y){
+    arr.push(y);
+    
+}
 function chk(x){
     document.getElementById("division").innerHTML = "you entered: "+x;
+    if(x==1){
+        document.getElementById("div3").style.display="block";
+    }
+    else if(x==2){
+        document.getElementById("div3").style.display="block";
+        document.getElementById("div4").innerHTML ="<p>Sed ut perspiciatis...</p>"
+    }
 }
