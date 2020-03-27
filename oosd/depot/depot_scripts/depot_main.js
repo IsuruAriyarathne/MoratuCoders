f98825fb8f5c3d1f59ce02ee59e170721c070d53
kavinda <kavindasr@icloud.com>
2020-03-28 00:25:25 +0530

    change js file

--- oosd/depot/depot_scripts/depot_main.js
+++ oosd/depot/depot_scripts/depot_main.js
@@ -1,11 +1,13 @@
 var x=0;
 var arr = [];
 
-function adder(y){
+function adder(){
+    let y = document.getElementById('inp2').value;
     arr.push(y);
     
 }
-function chk(x){
+function chk(){
+    x = document.getElementById('inp1').value;
     document.getElementById("division").innerHTML = "you entered: "+x;
     if(x==1){
         document.getElementById("div3").style.display="block";
