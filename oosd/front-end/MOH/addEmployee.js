34a51b4c544c1e7c9f707c3f1b4a935ea2876d24
Isuru Ariyarathne <61895498+IsuruAriyarathne@users.noreply.github.com>
2020-05-16 00:02:06 +0530

    post method

--- oosd/front-end/MOH/addEmployee.js
+++ oosd/front-end/MOH/addEmployee.js
@@ -1,3 +1,13 @@
-function popupmsg(){
-    var msg=prompt("Emoplyee removed successfully");
+var myObj;
+myObj = {
+  employee_id: 3346456,
+  salary_id: 334445,
+  name: "Isuru Ariyarathne",
+  dob: 19997 - 07 - 28,
+  employee_type: 1,
+  gender: M,
+};
+
+function popupmsg() {
+  var msg = prompt("Emoplyee removed successfully");
 }
