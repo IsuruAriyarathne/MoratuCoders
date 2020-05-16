3be7940e0c7131fb2c66809aa24caad41e8f7766
roshinie <roshinie.18@cse.mrt.ac.lk>
2020-05-16 18:58:44 +0530

    Employee detail view file changes

--- oosd/front-end/MOH/employeedetail.js
+++ oosd/front-end/MOH/employeedetail.js
@@ -1,22 +1,21 @@
-function loadDoc() {
+function viewEmployee() {
   var xhttp = new XMLHttpRequest();
+  var url = "http://localhost:8000/employees?id=" + document.getElementById("idnum").value;
   xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      empdetails = JSON.parse(this.responseText);
-      renderHtml(empdetails);
-    }
+      if (this.readyState == 4 && this.status == 200) {
+        var empdetails = JSON.parse(this.responseText);
+        console.log(empdetails);
+        renderHtml(empdetails);
+      }
   };
-  xhttp.open(
-    "GET",
-    "localhost:8000/employees?" + document.getElementById("idnum").value,
-    true
-  );
-  xhttp.send();
+xhttp.open("GET",url, true);
+xhttp.send();
 }
 
 function renderHtml(data) {
-  document.getElementById("empName").innerHTML = htmlString1;
-  document.getElementById("empSalaryID").innerHTML = htmlString2;
-  document.getElementById("empGender").innerHTML = htmlString3;
-  document.getElementById("empType").innerHTML = htmlString4;
+document.getElementById("empName").innerHTML = "Name  : "+data[0].name;
+document.getElementById("empID").innerHTML = "Employee ID  : "+data[0].employee_id;
+document.getElementById("empSalID").innerHTML = "Salary ID  : "+data[0].salary_id;
+document.getElementById("empGender").innerHTML = "Gender  : "+data[0].gender;
+document.getElementById("empType").innerHTML = "Employee Type  : "+data[0].employee_type;
 }
