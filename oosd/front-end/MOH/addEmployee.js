ddac3ae7db6c9734fb032454e063cf3a639dc7d4
roshinie <roshinie.18@cse.mrt.ac.lk>
2020-05-16 19:24:23 +0530

    Revert "Add Employee file changes"
    This reverts commit 9ecef595d7d7279beb10b94a70731a334cc1f575.

--- oosd/front-end/MOH/addEmployee.js
+++ oosd/front-end/MOH/addEmployee.js
@@ -1,29 +1,28 @@
-function submitAttendance() {
-    
-  var emp_Name= document.getElementById('name').value;
-  var emp_id = parseInt(document.getElementById('id').value);
-  var sal_id = parseInt(document.getElementById('salid').value);
-  var emp_dob = document.getElementById('dob').value;
-  var emp_gen = document.getElementById('selection').value;
-  var emp_type = parseInt(document.getElementById('emtype').value);
+function submitEmployee() {
+  var emp_Name = document.getElementById("name").value;
+  var emp_id = parseInt(document.getElementById("id").value);
+  var sal_id = parseInt(document.getElementById("salid").value);
+  var emp_dob = document.getElementById("dob").value;
+  var emp_gen = document.getElementById("selection").value;
+  var emp_type = parseInt(document.getElementById("emtype").value);
 
   var empObj = {
-                  employee_id: emp_id,
-                  salary_id: sal_id, 
-                  name: emp_Name, 
-                  dob : emp_dob,
-                  employee_type : emp_type,
-                  gender  : emp_gen
-                };
-  
-  console.log(empObj);
+    employee_id: emp_id,
+    salary_id: sal_id,
+    name: emp_Name,
+    dob: emp_dob,
+    employee_type: emp_type,
+    gender: emp_gen,
+  };
 
+  console.log(empObj);
+  alert(JSON.stringify(empObj));
   var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-          alert("Data submitted successfully..");
-      }
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      alert("New Employee added successfully..");
+    }
   };
   xhttp.open("POST", "http://localhost:8000/employees", true);
-  xhttp.send(JSON.stringify(empObj))
+  xhttp.send(JSON.stringify(empObj));
 }
