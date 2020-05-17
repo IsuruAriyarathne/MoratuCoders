4e947beffe706d290287c20330315b428a445d7b
kavinda <kavindasr@icloud.com>
2020-05-17 12:29:13 +0530

    depot_edit

---
+++ oosd/front-end/depot/depot_scripts/depot_edit.js
@@ -0,0 +1,22 @@
+function viewDailyAttendane() {
+    var tdate = document.getElementById('tdate').value;
+    var xhttp = new XMLHttpRequest();
+    var url = "http://localhost:8000/attendance?date=" + tdate;
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+          var dailyAttendance = JSON.parse(this.responseText);
+          console.log(dailyAttendance);
+          renderHtml(dailyAttendance);
+        }
+    };
+  xhttp.open("GET",url, true);
+  xhttp.send();
+  }
+  
+function renderHtml(data) {
+    htmlString = "<table><tr><th>Employee ID</th><th>Division></th><th>Vehicle/walk</th></tr>";
+    date.forEach(element => {
+        htmlString += "<tr>"+"<td>"+element.employee_id+"</td>"+"<td>"+element.division+"</td>"+"<td>"+element.vehiclewalk+"</td>"+"</tr>";
+    });
+    document.getElementById("display").innerHTML = htmlString;
+}
