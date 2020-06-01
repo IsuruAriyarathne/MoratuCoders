367015b04fa32aa2e4bfb2d4a7a3eec20d694829
kavinda <kavindasr@icloud.com>
2020-06-02 01:25:44 +0530

    front-end validation: check emp_id is vaild

--- oosd/front-end/depot/depot_scripts/depot_main.js
+++ oosd/front-end/depot/depot_scripts/depot_main.js
@@ -11,6 +11,25 @@ var attendList = []; //attended employee object array
 
 getVehiList();
 
+function empValidation(id){
+    return new Promise((res,rej)=>{
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4) {
+                if(this.status == 200){
+                    res(true);
+                }
+                else if(this.status == 400){
+                    res(false);
+                }       
+            }
+        };
+        xhttp.open("GET", "http://localhost:8000/employees?type=status&id="+id, true);
+        xhttp.send();
+    });
+    
+}
+
 function getVehiList(){
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
@@ -77,32 +96,41 @@ function removerV(){
 }
 
 
-function adder(){
-  
+async function adder(){
     let y = document.getElementById('inp2').value;
     let ind = arr.indexOf(y);
-    if(ind == -1){
-        arr.push(y);
-        var para = document.createElement("div");
-        var node = document.createTextNode(y);
-        para.appendChild(node);
-        var element = document.getElementById("divi");
-        element.appendChild(para);
+    if(ind == -1 && arrV.indexOf(y) == -1){
+        var bol = await empValidation(y);
+        if(bol){
+            arr.push(y);
+            var para = document.createElement("div");
+            var node = document.createTextNode(y);
+            para.appendChild(node);
+            var element = document.getElementById("divi");
+            element.appendChild(para);
+        }
+        else{
+            alert("Invaild ID")
+        } 
     }
     console.log(arr);
 }
 
-function adderV(){
-
+async function adderV(){
     let y = document.getElementById('inp4').value;
     let ind = arrV.indexOf(y);
-    if(ind == -1){
-        arrV.push(y);
-        var para = document.createElement("div");
-        var node = document.createTextNode(y);
-        para.appendChild(node);
-        var element = document.getElementById("divi2");
-        element.appendChild(para);
+    if(ind == -1 && arr.indexOf(y) == -1){
+        var bol = await empValidation(y);
+        if(bol){
+            arrV.push(y);
+            var para = document.createElement("div");
+            var node = document.createTextNode(y);
+            para.appendChild(node);
+            var element = document.getElementById("divi2");
+            element.appendChild(para);
+        }else{
+            alert("Invaild ID");
+        }  
     }
     console.log(arrV);
 }
