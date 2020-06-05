c607b6e139c507f7fa9c5c9e940bc4a456d695db
roshinie <roshinie.18@cse.mrt.ac.lk>
2020-06-05 16:19:40 +0530

    clerk interface changes

---
+++ oosd/front-end/clerk/clerk_scripts/clerk_gin.js
@@ -0,0 +1,8 @@
+function toggle(x){
+    if (x==0){
+        document.getElementById("bill").style.display="block";
+    }else{
+        document.getElementById("bill").style.display="none";
+        return;
+    }
+}
