f49a198f47e49163250a214fae13084c340f73cf
kavinda <kavindasr@icloud.com>
2020-06-06 02:31:07 +0530

    added object pool design pattern to solve 'too many connections'

--- oosd/back-end/core/db.js
+++ oosd/back-end/core/db.js
@@ -2,14 +2,14 @@ const mysql = require('mysql');
 const settings = require('../settings');
 
 exports.excutesql = (sql, callback)=>{
-    var sqlConnection = mysql.createConnection(settings.dbConfig);
-    sqlConnection.connect((err)=>{
+    var pool = mysql.createPool(settings.dbConfig);
+    pool.getConnection((err,conn)=>{
         if(err){
-            console.log(err);
             callback(null,err);
         }
         else{
-            sqlConnection.query(sql,(err,results)=>{
+            conn.query(sql,(err,results)=>{
+                conn.destroy();
                 if(err){
                     console.log(err);
                     callback(null,err);
@@ -20,5 +20,5 @@ exports.excutesql = (sql, callback)=>{
                 }
             });
         }
-   });
+    });
 };
